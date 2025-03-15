0bd8b2955ed6e3be3651b82ef84421c8494f9f70
Thiago Soares <thiagocarv4@gmail.com>
2025-03-15 17:54:01 -0300

    separando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { myMiddleware } from "./middlewares/my-middleware"
+import { routes } from "./routes/index"
 
 const PORT = 3333
 
@@ -7,19 +7,6 @@ const app = express()
 
 app.use(express.json())
 
-//app.use(myMiddleware)
-
-app.get("/products", (request, response) => {
-  const {page, limit} = request.query
-
-  response.send(`Página ${page} de ${limit}`)
-})
-
-app.post("/products", myMiddleware, (request, response) => {
-  const {name, price} = request.body
-
-  //response.send(`Produto ${name} custa $${price}`)
-  response.status(201).json({name, price, user_id: request.user_id})
-})
+app.use(routes)
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
