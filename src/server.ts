658253018ae2acc42e7ce569a71c49a7cddcc5b0
Thiago Soares <thiagocarv4@gmail.com>
2025-03-15 13:53:21 -0300

    Query Params

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,10 @@ const PORT = 3333
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+app.get("/products", (request, response) => {
+  const {page, limit} = request.query
 
-app.get("/products/:id/:user", (request, response) => {
-  const {id, user} = request.params
-
-  response.send(`Produto ${id} do usuário ${user}`)
+  response.send(`Página ${page} de ${limit}`)
 })
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
