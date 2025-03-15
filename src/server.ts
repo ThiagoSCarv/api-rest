faddc660b87da19f49f0a141278028fcf601f5dc
Thiago Soares <thiagocarv4@gmail.com>
2025-03-15 13:36:40 -0300

    Enviando requisição com o método GET

--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,7 @@ const PORT = 3333
 const app = express()
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+
+app.get("/", (request, response) => {
+  response.send("Hello World Express!")
+})
