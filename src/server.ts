f01ab9787a94720aa0eeab5036f3b43272012176
Thiago Soares <thiagocarv4@gmail.com>
2025-03-15 14:28:07 -0300

    Dados no corpo da requisição

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,18 @@ const PORT = 3333
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/products", (request, response) => {
   const {page, limit} = request.query
 
   response.send(`Página ${page} de ${limit}`)
+})
+
+app.post("/products", (request, response) => {
+  const {name, price} = request.body
+
+  response.send(`Produto ${name} custa $${price}`)
 })
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
