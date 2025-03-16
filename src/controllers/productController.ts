acaf8d9c89edb495fd76009a2218d72fd19a965e
Thiago Soares <thiagocarv4@gmail.com>
2025-03-16 09:20:22 -0300

    Tratando Requisição

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -16,6 +16,10 @@ class ProductsController{
 
     //throw new AppError("Erro ao tentar criar um produto")
 
+    if (!name || !price){
+      throw new AppError("Nome e preço do produto são obrigatórios!")
+    }
+
     response.status(201).json({ name, price, user_id: request.user_id });
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use(
-  (error: any, request: Request, response: Response, next: NextFunction) => {
+  (error: any, request: Request, response: Response, _: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({ message: error.message });
     }
