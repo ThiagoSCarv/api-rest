c1f26940efce2fee750f8342f6e1f44ae1253246
Thiago Soares <thiagocarv4@gmail.com>
2025-03-17 14:19:26 -0300

    Validando tipo de dado com ZOD

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,27 +1,25 @@
 import { Request, Response } from "express";
 import { AppError } from "../utils/appError";
+import { z } from "zod";
 
-class ProductsController{
-
-  index(request: Request, response: Response){
+class ProductsController {
+  index(request: Request, response: Response) {
     const { page, limit } = request.query;
-    
+
     response.send(`Página ${page} de ${limit}`);
   }
 
-  create(request: Request, response: Response){
-    const { name, price } = request.body;
+  create(request: Request, response: Response) {
+    
+    const bodySchema = z.object({
+      name: z.string(),
+      price: z.number(),
+    });
 
-    //throw new Error("Erro de Exemplo")
-
-    //throw new AppError("Erro ao tentar criar um produto")
-
-    if (!name || !price){
-      throw new AppError("Nome e preço do produto são obrigatórios!")
-    }
+    const { name, price } = bodySchema.parse(request.body);
 
     response.status(201).json({ name, price, user_id: request.user_id });
   }
 }
 
-export {ProductsController}
+export { ProductsController };
