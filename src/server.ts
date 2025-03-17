3ffa6aedac21f47d086c76693d5afb3f1132f79d
Thiago Soares <thiagocarv4@gmail.com>
2025-03-17 14:36:56 -0300

    Formatando mensagens de erro com ZOD

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { routes } from "./routes/index";
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "./utils/appError";
+import { ZodError } from "zod";
 
 const PORT = 3333;
 
@@ -11,14 +12,19 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use(
-  (error: any, request: Request, response: Response, _: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({ message: error.message });
-    }
-
-    response.status(500).json({ message: error.message });
+app.use((error: any, request: Request, response: Response, _: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message });
   }
-);
+
+  if (error instanceof ZodError) {
+    return response.status(400).json({
+      message: "Validation Error!",
+      issues: error.format(),
+    });
+  }
+
+  response.status(500).json({ message: error.message });
+});
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
